340b5fe4eabe8deee736f36cf2b300dede42c8d2
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 19:12:05 -0800

    Switch to left top

--- test/unit/tools/pdf-create/pdf-create.test.ts
+++ test/unit/tools/pdf-create/pdf-create.test.ts
@@ -66,8 +66,8 @@ describe('pdf-create tool', () => {
     const input: Input = {
       filename: 'shapes.pdf',
       content: [
-        { type: 'rect', position: 'absolute', x: 50, y: 50, width: 100, height: 100, fillColor: 'blue' },
-        { type: 'circle', position: 'absolute', x: 200, y: 100, radius: 50, fillColor: 'red' },
+        { type: 'rect', position: 'absolute', left: 50, top: 50, width: 100, height: 100, fillColor: 'blue' },
+        { type: 'circle', position: 'absolute', left: 200, top: 100, radius: 50, fillColor: 'red' },
         { type: 'line', position: 'absolute', x1: 50, y1: 200, x2: 250, y2: 200, strokeColor: 'green' },
       ],
     };
